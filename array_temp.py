14b84181870516c14e0d6c15fe500390149c6c25
CezaryBober95 <cezary.bober11@gmail.com>
2020-06-11 18:34:23 +0200

    2nd commit

--- array_temp.py
+++ array_temp.py
@@ -1,5 +1,7 @@
 import numpy as np
 import time
+import os.path
+
 #1)todo:class Generator
 #2)todo: plots
 #3)todo: random gekon position
@@ -18,30 +20,34 @@ animal=np.array([[22.4,22.45,22.54,22.67,22.87,22.45,22.75,22.64],
                  [22.65,22.76,22.65,22.65,22.45,22.46,22.98,22.56]])
 #GEKON -> animal[1][5] : animal [4][5]
 
+name= "Lizard"
+
 def coordinate(array):
     cor=np.where(array == array.max())
     array_i, array_j= cor[0], cor[1]
     array_i, array_j=int(array_i), int(array_j)
-    #print(array_i,array_j)
     return(array_i,array_j) # tuple
-    #array_value=array[array_i][array_j]
-    #print(array_value)
 
 def value_check(array,row,col):
     max_temp=(array[row][col])
     return max_temp
 
-def Raport(array,max):
-    raport = open("Raport.txt", "w")
+def Raport(array,max,name):
+    animal_name="Raport_%(name)s.txt" % {'name': name}
     start="AMG88xx pixels\n-- Pixels Test --\n\n"
     current_time = time.strftime("%Y-%m-%d %H:%M:%S")
     hour="\n\nCzas pomiaru: %s" % current_time
-    max_temp="\nMaksymalna temperatura: %s C" % str(max)
-    raport.write(start)
-    raport.write(str(array))
-    raport.write(hour)
-    raport.write(max_temp)
-    raport.close()
+    max_C="\nMaksymalna temperatura: %s C" % str(max)
+    if os.path.exists(animal_name):
+
+
+    else:
+        raport = open(animal_name, "w")
+        raport.write(start)
+        raport.write(str(array))
+        raport.write(hour)
+        raport.write(max_C)
+        raport.close()
 
 #cor_ij=coordinate(animal)
 #Raport(animal,value_check(animal,cor_ij[0],cor_ij[1]))
@@ -50,6 +56,7 @@ def Raport(array,max):
 
 for i in range(5):
     score_array = np.round(np.random.uniform(19.0, 27.0, (8, 8)),2)
-    cor_ij = coordinate(animal)
-    Raport(score_array, value_check(score_array, cor_ij[0], cor_ij[1]))
+    cor_ij = coordinate(score_array)
+    Raport(score_array, value_check(score_array, cor_ij[0], cor_ij[1]),name)
+    #time.sleep(1)
 
