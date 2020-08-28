56a93eb7ea12e739182051993ccecf008cdd97cd
CezaryBober95 <cezary.bober11@gmail.com>
2020-08-28 23:31:38 +0200

    Ploty 0.0.5 Score 0.0.2 Generator_GUI 0.1.1

--- plots_test.py
+++ plots_test.py
@@ -15,18 +15,16 @@ user_cols=['DataTime','Temp max','Temp min']
 # change file in date1 to get new plot
 #'Temp_raport_Dino.txt' -> 'Temp_raport_Denver'
 #file_me='Temp_raport_Test14.txt'
-#file_me=input("File name: ")
-date1=pd.read_table('Test.txt', sep=',',header=None,names=user_cols)
-#date1=pd.read_table(file_me, sep=',',header=None,names=user_cols)
+file_me=input("File name: ")
+#date1=pd.read_table('Temp_raport_Test14.txt', sep=',',header=None,names=user_cols)
+date1=pd.read_table(file_me, sep=',',header=None,names=user_cols)
 print(date1.head())
 print("Wczytane")
 
 date1['DataTime'] = date1['DataTime'].map(lambda x: datetime.strptime(str(x), '%Y-%m-%d %H:%M:%S'))
 x = date1['DataTime']
 y = date1['Temp max']
-z = date1['Temp min']
 
 plt.plot(x,y,c="red")
-plt.plot(x,z,c="blue")
 plt.gcf().autofmt_xdate()
 plt.show()
